a82f2c464a98be0806f5395e99ba3366decaa707
Talipot Technology Solutions LLP <82216231+TalipotTech@users.noreply.github.com>
2025-12-30 12:13:01 +0530

    Initial commit: Java tutorial commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    // The main method - entry point of the program
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java Programming!");
+    }
+}
